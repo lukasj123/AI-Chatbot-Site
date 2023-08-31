ca045b2eb0ab1b606b6bbfaf63ceee73f4bec5c3
lukasj123 <ljenkin3@u.rochester.edu>
2023-08-31 14:57:12 -0400

    Added railway postgres db

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'chatbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+POSTGRES_PASS = os.environ.get('POSTGRES_PASS')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': POSTGRES_PASS,
+        'HOST': 'containers-us-west-163.railway.app',
+        'PORT': '7592',
     }
 }
 
